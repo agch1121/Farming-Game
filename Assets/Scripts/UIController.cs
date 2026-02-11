9dd1dffeb3acc6c158c2d81e5a74043a9957fad0
agch1121 <155516846+agch1121@users.noreply.github.com>
2026-02-12 00:07:16 +0900

    [feat] 도구 UI 세팅
    - 도구 UI 배치
    - UI 관리 클래스 추가

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject[] toolbarActivatorIcons;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwitchTool(0);
+    }
+
+    public void SwitchTool(int selected)
+    {
+        foreach (GameObject icon in toolbarActivatorIcons)
+        {
+            icon.SetActive(false);
+        }
+
+        toolbarActivatorIcons[selected].SetActive(true);
+    }
+}
